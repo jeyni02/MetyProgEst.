b68a0fee3969d9ed891fd42a53e1aaef44a5d80a
jeyni02 <jeynilomary@uamv.edu.ni>
2024-08-25 16:54:50 -0600

    Add files via upload

---
+++ ÁreaDeUnCirculo/ÁreaDeUnCirculo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ÁreaDeUnCirculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ÁreaDeUnCirculo calculadora = new ÁreaDeUnCirculo();
+
+            Console.Write("Ingresa el radio del círculo: ");
+            calculadora.Radio = double.Parse(Console.ReadLine());
+
+            double area = calculadora.CalcularAreaCirculo();
+            Console.WriteLine($"\nEl área del círculo con radio {calculadora.Radio} es: {area}");
+        }
+    }
+}
---
+++ ÁreaDeUnCirculo/ÁreaDeUnCirculo/clase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ÁreaDeUnCirculo
+{
+    internal class ÁreaDeUnCirculo
+    {
+        public double Radio { get; set; }
+
+        public double CalcularAreaCirculo()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+    }
+}
