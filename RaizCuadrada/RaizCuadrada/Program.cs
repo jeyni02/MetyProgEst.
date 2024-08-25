023ea15adb402205f78c24f2fb4594b80fe3f283
jeyni02 <jeynilomary@uamv.edu.ni>
2024-08-25 16:54:11 -0600

    Add files via upload

---
+++ RaizCuadrada/RaizCuadrada/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RaizCuadrada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RaizCuadrada calculadora = new RaizCuadrada();
+
+            Console.Write("Ingresa un número: ");
+            calculadora.Numero = double.Parse(Console.ReadLine());
+
+            double raizCuadrada = calculadora.CalcularRaizCuadrada();
+            Console.WriteLine($"\nLa raíz cuadrada de {calculadora.Numero} es: {raizCuadrada}");
+        }
+    }
+}
---
+++ RaizCuadrada/RaizCuadrada/clase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RaizCuadrada
+{
+    internal class RaizCuadrada
+    {
+        public double Numero { get; set; }
+
+        public double CalcularRaizCuadrada()
+        {
+            return Math.Sqrt(Numero);
+        }
+    }
+}
