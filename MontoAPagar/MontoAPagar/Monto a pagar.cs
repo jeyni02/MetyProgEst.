f01fd4e18de9ce1a08153a1c314c69d95a94e913
jeyni02 <jeynilomary@uamv.edu.ni>
2024-08-25 16:51:37 -0600

    Add files via upload

---
+++ MontoAPagar/MontoAPagar/Monto a pagar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MontoAPagar
+{
+    internal class Monto_a_pagar
+    {
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+
+        public double CalcularMontoTotal()
+        {
+            return Precio * Cantidad;
+        }
+    }
+}
---
+++ MontoAPagar/MontoAPagar/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MontoAPagar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Monto_a_pagar compra = new Monto_a_pagar();
+
+            Console.Write("Ingresa el precio del producto: ");
+            compra.Precio = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa la cantidad de productos: ");
+            compra.Cantidad = int.Parse(Console.ReadLine());
+
+            double montoTotal = compra.CalcularMontoTotal();
+            Console.WriteLine($"\nEl monto total a pagar es: {montoTotal}");
+        }
+    }
+}
