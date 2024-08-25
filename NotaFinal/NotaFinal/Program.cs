0cfdc7719acf910d528e5b86b6de16daf6a67d4c
jeyni02 <jeynilomary@uamv.edu.ni>
2024-08-25 16:52:46 -0600

    Add files via upload

---
+++ NotaFinal/NotaFinal/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NotaFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotaFinal estudiante = new NotaFinal();
+
+            Console.Write("Ingresa la primera nota: ");
+            estudiante.Nota1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa la segunda nota: ");
+            estudiante.Nota2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingresa la tercera nota: ");
+            estudiante.Nota3 = double.Parse(Console.ReadLine());
+
+            double notaFinal = estudiante.CalcularNotaFinal();
+            Console.WriteLine($"\nLa nota final del estudiante es: {notaFinal}");
+        }
+    }
+}
---
+++ NotaFinal/NotaFinal/clase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NotaFinal
+{
+    internal class NotaFinal
+    {
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+        public double CalcularNotaFinal()
+        {
+            return (Nota1 + Nota2 + Nota3) / 3;
+        }
+    }
+}
