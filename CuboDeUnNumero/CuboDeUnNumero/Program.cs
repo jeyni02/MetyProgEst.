227ba78035d6f07e418a7516e6f1be8da79efcd2
jeyni02 <jeynilomary@uamv.edu.ni>
2024-08-25 16:53:41 -0600

    Add files via upload

---
+++ CuboDeUnNumero/CuboDeUnNumero/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CuboDeUnNumero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CuboDeUnNumero calculadora = new CuboDeUnNumero();
+
+            Console.Write("Ingresa un número: ");
+            calculadora.Numero = double.Parse(Console.ReadLine());
+
+            double cubo = calculadora.CalcularCubo();
+            Console.WriteLine($"\nEl cubo del número {calculadora.Numero} es: {cubo}");
+        }
+    }
+}
---
+++ CuboDeUnNumero/CuboDeUnNumero/clase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CuboDeUnNumero
+{
+    internal class CuboDeUnNumero
+    {
+        public double Numero { get; set; }
+
+        public double CalcularCubo()
+        {
+            return Math.Pow(Numero, 3);
+        }
+    }
+}
